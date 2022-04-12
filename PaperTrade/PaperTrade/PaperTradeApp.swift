03f64ca1ad92e679725579fbac0b19a83a0cdee0
Kai <kwang339@wisc.edu>
2022-04-12 12:17:59 -0700

    init swiftUI

---
+++ PaperTrade/PaperTrade/PaperTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperTradeApp.swift
+//  PaperTrade
+//
+//  Created by Kai Wang on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PaperTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
